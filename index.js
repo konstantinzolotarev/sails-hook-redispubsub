04e64dbe4945139d47e736ce6f5bfc8b956ff396
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-10-05 18:04:02 +0600

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (sails) {
+
+  return {
+
+  	defaults: require('lib/defaults'),
+
+    initialize: function (cb) {
+      
+      cb();
+    }
+
+  };
+};
